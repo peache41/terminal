79a1292a9cc933a0642a948a9299f8bb7caf1f97
Jack3113 <jack03113@gmail.com>
2016-12-07 07:00:18 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,196 @@
+/**
+ * Created by Jean on 06/12/2016.
+ */
+var printableCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '0',
+    '\'',
+    '"',
+    '?',
+    '/',
+    '-',
+    ':',
+    '\\',
+    '~',
+    '&',
+    '=',
+    '+',
+    '(',
+    ')',
+    '{',
+    '}',
+    '[',
+    ']',
+    '|',
+    '_',
+    '*',
+    '$',
+    '%',
+    '!',
+    ';',
+    ',',
+    '.',
+    ' ',
+    '@'
+];
+
+var prompt = '';
+var display = [];
+var identifier = 'hack@jack:~# ';
+var version = 'v0.0.4';
+
+var commands = {
+    man: {
+        manual: ['Display the manual of the command'],
+        bin: function(args) {
+            var f = args[0];
+            if (commands.hasOwnProperty(f)) {
+                commands[f].manual.forEach(function(line) {
+                    displayLine(line);
+                });
+            } else {
+                displayLine('man: ' + f + ': command not found');
+            }
+        }
+    },
+    alert: {
+        manual: ['Exectute a JavaScript alert'],
+        bin: function(args) {
+            alert(args.join(' '));
+        }
+    }
+};
+
+function toHTML(str) {
+    return str.replace(/&/gi, '&amp;').replace(/ /gi, '&nbsp;').replace(/"/gi, '&quot;');
+}
+
+function print() {
+    var promptElmt = document.getElementById('keyboard');
+    promptElmt.innerHTML = toHTML(prompt);
+}
+
+function displayLine(line) {
+    display.push(line);
+    printHistory();
+}
+
+function printHistory() {
+    var historyElmt = document.getElementById('history');
+    var output = display.map(function(line) {
+        return toHTML(line);
+    });
+    historyElmt.innerHTML = output.join('<br/>');
+    historyElmt.scrollTop = historyElmt.scrollHeight;
+}
+
+function addChar(char) {
+    prompt += char;
+    print();
+}
+
+function deleteLastChar() {
+    if (prompt.length > 0) {
+        prompt = prompt.slice(0, -1);
+        print();
+    }
+}
+
+function exec(command) {
+    var input = command.split(' ');
+    var f = input.shift();
+    if (commands.hasOwnProperty(f)) {
+        commands[f].bin(input);
+    } else {
+        displayLine('hackterm: ' + f + ': command not found');
+    }
+}
+
+function cleanPrompt() {
+    displayLine(identifier + prompt);
+    if (prompt.length > 0) {
+        exec(prompt);
+    }
+    prompt = '';
+    print();
+    printHistory();
+}
+
+function load() {
+    displayLine('HHHHHHHHH     HHHHHHHHH                                     kkkkkkkk           TTTTTTTTTTTTTTTTTTTTTTT                                                            ');
+    displayLine('H:::::::H     H:::::::H                                     k::::::k           T:::::::::::::::::::::T                                                            ');
+    displayLine('H:::::::H     H:::::::H                                     k::::::k           T:::::::::::::::::::::T                                                            ');
+    displayLine('HH::::::H     H::::::HH                                     k::::::k           T:::::TT:::::::TT:::::T                                                            ');
+    displayLine('  H:::::H     H:::::H    aaaaaaaaaaaaa      cccccccccccccccc k:::::k    kkkkkkkTTTTTT  T:::::T  TTTTTTeeeeeeeeeeee    rrrrr   rrrrrrrrr      mmmmmmm    mmmmmmm   ');
+    displayLine('  H:::::H     H:::::H    a::::::::::::a   cc:::::::::::::::c k:::::k   k:::::k         T:::::T      ee::::::::::::ee  r::::rrr:::::::::r   mm:::::::m  m:::::::mm ');
+    displayLine('  H::::::HHHHH::::::H    aaaaaaaaa:::::a c:::::::::::::::::c k:::::k  k:::::k          T:::::T     e::::::eeeee:::::eer:::::::::::::::::r m::::::::::mm::::::::::m');
+    displayLine('  H:::::::::::::::::H             a::::ac:::::::cccccc:::::c k:::::k k:::::k           T:::::T    e::::::e     e:::::err::::::rrrrr::::::rm::::::::::::::::::::::m');
+    displayLine('  H:::::::::::::::::H      aaaaaaa:::::ac::::::c     ccccccc k::::::k:::::k            T:::::T    e:::::::eeeee::::::e r:::::r     r:::::rm:::::mmm::::::mmm:::::m');
+    displayLine('  H::::::HHHHH::::::H    aa::::::::::::ac:::::c              k:::::::::::k             T:::::T    e:::::::::::::::::e  r:::::r     rrrrrrrm::::m   m::::m   m::::m');
+    displayLine('  H:::::H     H:::::H   a::::aaaa::::::ac:::::c              k:::::::::::k             T:::::T    e::::::eeeeeeeeeee   r:::::r            m::::m   m::::m   m::::m');
+    displayLine('  H:::::H     H:::::H  a::::a    a:::::ac::::::c     ccccccc k::::::k:::::k            T:::::T    e:::::::e            r:::::r            m::::m   m::::m   m::::m');
+    displayLine('HH::::::H     H::::::HHa::::a    a:::::ac:::::::cccccc:::::ck::::::k k:::::k         TT:::::::TT  e::::::::e           r:::::r            m::::m   m::::m   m::::m');
+    displayLine('H:::::::H     H:::::::Ha:::::aaaa::::::a c:::::::::::::::::ck::::::k  k:::::k        T:::::::::T   e::::::::eeeeeeee   r:::::r            m::::m   m::::m   m::::m');
+    displayLine('H:::::::H     H:::::::H a::::::::::aa:::a cc:::::::::::::::ck::::::k   k:::::k       T:::::::::T    ee:::::::::::::e   r:::::r            m::::m   m::::m   m::::m');
+    displayLine('HHHHHHHHH     HHHHHHHHH  aaaaaaaaaa  aaaa   cccccccccccccccckkkkkkkk    kkkkkkk      TTTTTTTTTTT      eeeeeeeeeeeeee   rrrrrrr            mmmmmm   mmmmmm   mmmmmm');
+    displayLine(' ');
+    displayLine('http://hackjack.info');
+    displayLine(version);
+    displayLine(' ');
+    var now = new Date();
+    displayLine(now.toString());
+    displayLine(' ');
+
+    document.getElementById('identifier').innerHTML = identifier;
+}
+
+window.addEventListener('load', function() {
+    load();
+    window.addEventListener('keyup', function(event) {
+        if (event.key) {
+            if (printableCharacters.indexOf(event.key) > -1) {
+                addChar(event.key);
+            }
+            else if (event.key === "Backspace") {
+                deleteLastChar();
+            }
+            else if (event.key === "Enter") {
+                cleanPrompt();
+            }
+        }
+    });
+}, false);
